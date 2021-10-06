1f10e97f7898f95e1e6ce1f994fa0c66f6f78cef
Alex <kolesnikaleksv@gmail.com>
2021-10-06 18:37:27 +0300

    Work with elements on the page

---
+++ script.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+
+// $('#btn'); // we get the element by ID
+// const btn = $('#btn');
+
+// console.log(btn);
+
+$(document).ready(function() {
+    $('.list-item:first').hover(function() {
+        $(this).toggleClass('active');
+    });
+    $('.list-item:eq(2)').on('click', () => {
+        $('.image:even').fadeToggle('slow');
+    });
+    $('.list-item:eq(4)').on('click', () => {
+        $('.image:odd').animate({
+            opacity: 'toggle',
+            height: 'toggle'
+        },3000);
+    });
+});
